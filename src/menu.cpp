e09da34a67c1ced040b2bc76debe0a557d6bda0e
Martin Benes <xbenes49@stud.fit.vutbr.cz>
2018-04-22 09:18:00 +0200

    buttons added!

--- src/menu.cpp
+++ src/menu.cpp
@@ -10,43 +10,74 @@
 #include "defs.h"
 #include "menu.h"
 
+void Menu::createButton(QString s, bool checkable = true)
+{
+    // create button
+    QPushButton* btn = new QPushButton(this);
+    btn->setText(s);
+    btn->setCheckable(checkable);
+    mButtons.insert( std::make_pair(s, btn) );
+
+    // get to scene
+    mlayout->addWidget(btn);
+
+    // map signals
+    mmapper.setMapping(btn, s);
+    QObject::connect(btn, SIGNAL(pressed()), &mmapper, SLOT(map()));
+}
+
+constexpr auto loadBtn = "Load";
+constexpr auto saveBtn = "Save";
+constexpr auto exitBtn = "Exit";
+constexpr auto wireBtn = "Wire";
+
 Menu::Menu(QWidget* parent): QWidget(parent)
 {
-    QVBoxLayout * layout = new QVBoxLayout(this);
-    layout->setAlignment(Qt::AlignTop);
+    mlayout = new QVBoxLayout(this);
+    mlayout->setAlignment(Qt::AlignTop);
 
+    // load & save
+    createButton(loadBtn, false);
+    createButton(saveBtn, false);
+    createButton(exitBtn, false);
+
+    QSpacerItem* s1 = new QSpacerItem(1,20);
+    mlayout->addSpacerItem(s1);
+
+    // blocks
     auto& names = Config::getBlockNames();
     for(auto& it: names)
     {
         QString s = QString::fromStdString(it.first);
-
-        // create button
-        QPushButton* btn = new QPushButton(this);
-        btn->setText(s);
-        btn->setCheckable(true);
-        mButtons.insert( std::make_pair(s, btn) );
-
-        // get to scene
-        layout->addWidget(btn);
-
-        // map signals
-        mmapper.setMapping(btn, s);
-        QObject::connect(btn, SIGNAL(pressed()), &mmapper, SLOT(map()));
+        createButton(s);
     }
 
+    QSpacerItem* s2 = new QSpacerItem(1,20);
+    mlayout->addSpacerItem(s2);
+
+    // wire
+    createButton(wireBtn);
+
     QObject::connect(&mmapper, SIGNAL(mapped(QString)), this, SLOT(slotChoicePressed(QString)));
-    setLayout(layout);
+    setLayout(mlayout);
 }
 
 void Menu::slotChoicePressed(QString name)
 {
+
+    if(name == loadBtn) { slotLoad(); return; }
+    else if(name == saveBtn) { slotSave(); return; }
+    else if(name == exitBtn) { slotExit(); return; }
+    
+    for(auto& it: mButtons) { if(it.first != name) it.second->setChecked(false); }
+    if(name == wireBtn) { emit sigWireMode(); return; }
+
     auto m = Config::getBlockNames();
     #warning wtf logic?
     long key = (!mButtons.at(name)->isChecked()) ?
                     m.at(name.toStdString()) : -1;
     emit sigChoiceMode(key);
 
-    for(auto& it: mButtons) { if(it.first != name) it.second->setChecked(false); }
     // std::cout << name.toStdString() << "\n";
 }
 
--- src/menu.h
+++ src/menu.h
@@ -1,12 +1,14 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include <iostream>
 #include <map>
 #include <string>
 
 #include <QPushButton>
 #include <QSignalMapper>
 #include <QString>
+#include <QVBoxLayout>
 #include <QWidget>
 
 class Menu: public QWidget
@@ -14,15 +16,22 @@ class Menu: public QWidget
     Q_OBJECT
     public:
         Menu(QWidget* parent = 0);
+        void createButton(QString, bool);
    
     public slots:
         void slotChoicePressed(QString);
         void slotChoiceRejected();
+
+        void slotLoad() { std::cerr << "Load a scheme.\n"; }
+        void slotSave() { std::cerr << "Save the scheme.\n"; }
+        void slotExit() { exit(0); }
     signals:
         void sigChoiceMode(long);
+        void sigWireMode();
     private:
         QSignalMapper mmapper;
         std::map<QString, QPushButton*> mButtons;
+        QVBoxLayout* mlayout;
 };
 
 #endif // MENU_H
